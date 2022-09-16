17e579a9ff8f5866feb5f75e03d83500f52c5174
Medynskyy Ostap <medinsky_os@groupbwt.com>
2022-09-16 10:29:46 +0300

    remove pagination if select category

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ export const App = () => {
           page={newPage}
           limit={limit}
           setNewPage={setNewPage}
+          newsByCategory={newsByCategory}
         />
       </div>
     </>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,21 +2,29 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export const PaginationButtons = ({ count, page, setNewPage, limit }) => {
+export const PaginationButtons = ({
+  count,
+  page,
+  setNewPage,
+  limit,
+  newsByCategory,
+}) => {
   const handlePage = (event, newPage) => {
     setNewPage(newPage);
   };
 
   return (
-    <Stack spacing={2}>
-      <Pagination
-        count={count / limit}
-        onChange={handlePage}
-        hidePrevButton
-        hideNextButton
-        color='primary'
-        page={page}
-      />
-    </Stack>
+    !newsByCategory.id && (
+      <Stack spacing={2}>
+        <Pagination
+          count={count / limit}
+          onChange={handlePage}
+          hidePrevButton
+          hideNextButton
+          color='primary'
+          page={page}
+        />
+      </Stack>
+    )
   );
 };
